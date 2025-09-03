68fc02121105d865ac249475185a0395b5dc9711
kaverimyakala <myakalakaveri@gmail.com>
2025-09-03 12:09:14 -0400

    Concatenation & Interpolation

--- 02_Variables/demo.py
+++ 02_Variables/demo.py
@@ -35,3 +35,39 @@ print(type(student_roll_no))
 print(type(student1_marks))  
 
 #Any object of a Class is Variable 
+
+data_one = "Good"
+data_two = " Morning"
+print(data_one + data_two)  #Concatenation : joining multiple strings together using '+' Operator
+
+num1 = 20
+num2 = 30
+print(num1 + num2) # Addition Perform mathmatical addition operation 
+
+# print (num1 + data_two) #unsupported operand type(s) for +: 'int' and 'str' it can not do string and integer both should be intg or str
+
+name = "kaveri"
+age = 22
+cgpa = 7.5
+print(f"my name is {name} my age is {age} and my cgpa is {cgpa}" ) # INTERPOLATION IS replacing placeholders with actual values
+
+x = 10 
+y = 20 
+z = 30
+print(x)
+print(y)
+print(z)
+
+x,y,z = 10,20,30
+print(x,y,z)
+
+x=y=z = 10
+print(x,y,z)
+
+x = 10
+y = 20
+z = 30
+print(x,y,z)
+
+'''x,y,z = 10,20,30,40
+print(x,y,z) #too many values to unpack (expected 3) there should be LHS == RHS'''
