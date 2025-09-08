8908c617a23d8b07d1208dd99b318ba9486f2364
kaverimyakala <myakalakaveri@gmail.com>
2025-09-08 02:47:42 -0400

    Nested Condition

---
+++ 03_Control_stmts/nested.py
@@ -0,0 +1,14 @@
+# Nested if-else
+
+age = int(input("enter age: ")) 
+has_id = input
+if age >= 18:         
+    print("Yes, You are eligible to vote")
+    if has_id == "yes":
+        print("yes your eligible to vote.")
+    else:
+        print("you need a voter id to vote.")
+else:
+    print("No, You are not eligible to vote")
+                        
+
